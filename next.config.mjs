e17523d79710bb158b2d90f78876c2607492a989
marlinlm <406043808@qq.com>
2024-06-23 23:07:30 +0800

    updated link

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "",
-  // basePath: "qianmo-home",
+  // basePath: "",
+  basePath: "qianmo-home",
   output: "export",  // <=== enables static exports
   reactStrictMode: true
   // swcMinify: false,
